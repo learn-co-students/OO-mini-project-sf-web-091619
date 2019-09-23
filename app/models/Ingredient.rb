e347f0dfaf1b9a4fe115a07d6e1660450b74cbca
Max Zakzook <mzakzook@me.com>
2019-09-23 13:54:34 -0700

    Done.

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,20 @@
-
+class Ingredient
+    ​
+    @@all = []
+​
+    def initialize
+        @@all << self
+    end
+​
+    def self.all
+        @@all
+    end
+​
+    def self.most_common_allergy
+        al = Allergy.all.map do |allergy|
+            allergy.ingredient
+        end
+        al.mode
+    end
+    ​
+end
